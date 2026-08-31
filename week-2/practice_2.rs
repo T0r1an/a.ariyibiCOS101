8da12161dd80be306277af40f519433e77fb9748
Ayomide Ariyibi <ayomide.ariyibi@pau.edu.ng>
2026-08-31 23:51:48 +0100

    Add week 2 practical work

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	
+	println!(); // prints just a 
+	
+	println!("hello ");//prints
+
+    println!("format {} arguements", "some"); // prints format some arguements
+
+    println!("My name is {}. I am the normal one", "AYOMIDE JOHN ARIYIBI");
+
+}
